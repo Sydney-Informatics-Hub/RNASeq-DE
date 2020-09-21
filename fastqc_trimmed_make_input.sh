3ad71220c42a77fb988016aa11807c6a91bea188
Tracy Chew <tracy.chew@sydney.edu.au>
2020-09-21 10:40:16 +1000

    Update scripts

---
+++ fastqc_trimmed_make_input.sh
@@ -0,0 +1,66 @@
+#! /bin/bash
+
+#########################################################
+#
+# Platform: NCI Gadi HPC
+# Description: Creates input file for fastqc_run_parallel.pbs
+#  for all samples in <cohort>.config
+# Files are processed from largest to smallest for optimum E.
+# Usage: Create <cohort>.config file using template.
+# sh fastqc_make_input.sh <cohort>
+# Author: Tracy Chew
+# tracy.chew@sydney.edu.au
+# Date last modified: 27/08/2020
+#
+# If you use this script towards a publication, please acknowledge the
+# Sydney Informatics Hub (or co-authorship, where appropriate).
+#
+# Suggested acknowledgement:
+# The authors acknowledge the scientific and technical assistance
+# <or e.g. bioinformatics assistance of <PERSON>> of Sydney Informatics
+# Hub and resources and services from the National Computational
+# Infrastructure (NCI), which is supported by the Australian Government
+# with access facilitated by the University of Sydney.
+#
+########################################################
+
+if [ -z "$1" ]
+then
+	echo "Please run this script with the base name of your config file, e.g. sh bbduk_trim_make_input.sh samples"
+	exit
+fi
+
+cohort=$1
+config=../$cohort.config
+logs=./Logs/fastQC_trimmed
+INPUTS=./Inputs
+unsorted=${INPUTS}/fastqc_trimmed.inputs_unsorted
+input_file=${INPUTS}/fastqc_trimmed.inputs
+NCPUS=1
+
+mkdir -p ${INPUTS}
+mkdir -p ${logs}
+
+rm -rf ${input_file}
+
+while read -r fastq sampleid dataset reference seqcentre platform run_type library; do
+	if [[ ! ${fastq} =~ ^#.*$ ]]; then
+		basename=$(basename "$fastq" | cut -d. -f1)
+		in=../${dataset}_trimmed/${fastq}
+		logdir=${logs}/${cohort}/${dataset}_trimmed
+		logfile=${logdir}/${basename}.oe
+		out=../${dataset}\_fastQC_trimmed
+		bytes=$(ls -s "${in}" | awk '{print $1}')
+
+		mkdir -p ${out}
+		mkdir -p ${logdir}
+
+		echo "${in},${out},${logfile},${NCPUS},${bytes}" >> ${unsorted}
+	fi
+done < "${config}"
+
+# Reverse numeric sort bytes, comma delimited unsorted file
+sort -rnk 5 -t ',' ${unsorted} > ${input_file}
+rm -rf ${unsorted}
+
+echo "Number of tasks for fastqc_run_parallel.pbs: $(wc -l ${input_file} | awk '{print $1}')"
--- multiqc.sh
+++ multiqc.sh
@@ -1,6 +1,28 @@
 #! /bin/bash
 
-module load python2
+#########################################################
+#
+# Platform: NCI Gadi HPC
+# Description: Run multiQC after running fastqc_run_parallel.pbs
+# for <cohort>.config
+# Usage: sh multiqc_all_datasets <cohort> 
+# Author: Tracy Chew
+# tracy.chew@sydney.edu.au
+# Date last modified: 27/08/2020
+#
+# If you use this script towards a publication, please acknowledge the
+# Sydney Informatics Hub (or co-authorship, where appropriate).
+#
+# Suggested acknowledgement:
+# The authors acknowledge the scientific and technical assistance
+# <or e.g. bioinformatics assistance of <PERSON>> of Sydney Informatics
+# Hub and resources and services from the National Computational
+# Infrastructure (NCI), which is supported by the Australian Government
+# with access facilitated by the University of Sydney.
+#
+#########################################################
+
+module load python3/3.8.5
 
 if [ -z "$1" ]
 then
@@ -9,7 +31,22 @@ then
 fi
 
 cohort=$1
-fastqc=../${cohort}\_fastQC
+config=../$cohort.config
 
-echo "Running multiQC for fastQC files in ${fastqc}..."
-multiqc --interactive ${fastqc} -o ${fastqc}
+# Get all unique datasets
+while read -r fastq sampleid dataset reference seqcentre platform run_type library; do
+	if [[ ! ${fastq} =~ ^#.*$ ]]; then
+		fastqc=../${dataset}_fastQC
+		datasets+=(${fastqc})	
+	fi
+done < "${config}"
+
+# Unique datasets
+uniq_datasets=($(echo "${datasets[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+
+echo "$(date): Performing multiqc for ${#uniq_datasets[@]} datasets: "${uniq_datasets[@]}""
+
+for dataset in "${uniq_datasets[@]}"; do
+	echo "$(date): MultiQC is running for ${dataset}..."
+	multiqc --interactive ${dataset} -o ${dataset}
+done
